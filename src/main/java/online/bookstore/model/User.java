2141eb2c42f98cb4bac08d83498644a5f82c6e4f
OD <sdenisenko055@gmail.com>
2024-10-23 21:49:16 +0300

    added spring-boot-starter-security dependency added SecurityConfig with PasswordEncoder and SecurityFilterChain added realization of UserDetailsService and UserDetails(in User class)

--- src/main/java/online/bookstore/model/User.java
+++ src/main/java/online/bookstore/model/User.java
@@ -7,10 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import java.util.Collection;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @SQLDelete(sql = "UPDATE users SET is_deleted = TRUE WHERE id = ?")
@@ -18,7 +22,7 @@ import org.hibernate.annotations.SQLRestriction;
 @Getter
 @Setter
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -33,4 +37,39 @@ public class User {
     private String shippingAddress;
     @Column(columnDefinition = "TINYINT(1)", nullable = false)
     private boolean isDeleted;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !isDeleted;
+    }
 }
