f0f6202fe0b3ef836c8692bf941b3b0968f75fc5
OD <sdenisenko055@gmail.com>
2024-10-24 17:03:30 +0300

    changed the thrown exception

--- src/main/java/online/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/online/bookstore/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import online.bookstore.dto.user.UserRegistrationRequestDto;
 import online.bookstore.dto.user.UserResponseDto;
+import online.bookstore.exception.RegistrationException;
 import online.bookstore.mapper.UserMapper;
 import online.bookstore.model.Role;
 import online.bookstore.model.User;
@@ -28,7 +29,7 @@ public class UserServiceImpl implements UserService {
         User user = userMapper.toModel(requestDto);
         user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
         user.setRoles(Set.of(roleRepository.findByName(Role.RoleName.ROLE_USER)
-                .orElseThrow(() -> new RuntimeException("Can't find role"))));
+                .orElseThrow(() -> new RegistrationException("Can't find role"))));
         return userMapper.toDto(userRepository.save(user));
     }
 }
