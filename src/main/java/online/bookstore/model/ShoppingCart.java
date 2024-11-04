8f63e6bcf8f519b259d3aee59d99bc5b861182ac
OD <sdenisenko055@gmail.com>
2024-11-04 17:07:03 +0200

    fixed to return testing capability
    (error "field id/user_id doesn't have a default value" was fixed)
    added generated value to ShoppingCart id field
    changed user id column name

--- src/main/java/online/bookstore/model/ShoppingCart.java
+++ src/main/java/online/bookstore/model/ShoppingCart.java
@@ -4,6 +4,8 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
@@ -25,10 +27,11 @@ import org.hibernate.annotations.SQLRestriction;
 @Table(name = "carts")
 public class ShoppingCart {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
-    @JoinColumn(name = "id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
     @OneToMany(mappedBy = "shoppingCart",
             cascade = CascadeType.ALL,
